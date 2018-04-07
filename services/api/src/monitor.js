b73c9e9b9e20086276c35de6cd082c107d74523a
Freek <freekpst@gmail.com>
2018-04-07 21:31:33 +0200

    Fix eslint issues

--- services/api/src/monitor.js
+++ services/api/src/monitor.js
@@ -1,57 +1,46 @@
-var Web3 = require('web3');
-var contract = require("truffle-contract");
+const Web3 = require('web3');
+const contract = require("truffle-contract");
 const Order = require('./models/order');
 
-var web3;
+let web3;
 const TomatoArtifact = require('./contracts/TomatoesMarket.json');
-
-const orderEnum = ['Placed', 'Executing', 'Completed'];
-
 
 async function start() {
   try {
     if (typeof web3 !== 'undefined') {
       web3 = new Web3(web3.currentProvider);
     } else {
-      // set the provider you want from Web3.providers
-      web3 = new Web3(new Web3.providers.HttpProvider("http://ganache:8545"));
+      web3 = new Web3(new Web3.providers.HttpProvider('http://ganache:8545'));
     }
     const tomatoContract = contract(TomatoArtifact);
     tomatoContract.setProvider(web3.currentProvider);
-    //console.log(tomatoContract);  
     const tomatoInstance = await tomatoContract.deployed();
-    const THISISWEB3 = web3.eth.contract(TomatoArtifact.abi);
+    const web3Contract = web3.eth.contract(TomatoArtifact.abi);
     
-    var contractInstance = THISISWEB3.at(tomatoInstance.address);
+    const contractInstance = web3Contract.at(tomatoInstance.address);
     console.log(contractInstance);
 
-    var eventNewOrder = contractInstance.LogNewOrder({}, {fromBlock: 0, toBlock: 'latest'});
-    eventNewOrder.watch(function(error, result){
-      console.log('ERROR', error, 'RESULT', result.args);
-
+    const eventNewOrder = contractInstance.LogNewOrder({}, { fromBlock: 0, toBlock: 'latest' });
+    eventNewOrder.watch((error, result) => {
+      //Do we add the new order to mongo here?
     });
 
-    var eventExecutingOrder = contractInstance.LogExecutingOrder({}, {fromBlock: 0, toBlock: 'latest'});
-    eventNewOrder.watch(function(error, result){
+    const eventExecutingOrder = contractInstance.LogExecutingOrder({}, { fromBlock: 0, toBlock: 'latest' });
+    eventNewOrder.watch((error, result) => {
       const order = Order.findOne({ orderId: result.args.orderId });
       order.state = 'Executing';
       order.save();
     });
 
-    var eventCompletedOrder = contractInstance.LogCompletedOrder({}, {fromBlock: 0, toBlock: 'latest'});
-    eventCompletedOrder.watch(function(error, result){
+    const eventCompletedOrder = contractInstance.LogCompletedOrder({}, { fromBlock: 0, toBlock: 'latest' });
+    eventCompletedOrder.watch((error, result) => {
       const order = Order.findOne({ orderId: result.args.orderId });
       order.state = 'Completed';
       order.save();
     });
-  }
-  catch(e) {
+  } catch (e) {
     console.log(e);
   }
-  
 }
-
-
-
 
 start();
