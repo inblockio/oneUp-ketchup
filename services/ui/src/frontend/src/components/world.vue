da44391078be23b0a61c462f3afc52a39e22efea
sunfeld <sunfeld@sunis.nl>
2018-04-07 15:45:57 +0200

    Added drones

--- services/ui/src/frontend/src/components/world.vue
+++ services/ui/src/frontend/src/components/world.vue
@@ -62,6 +62,7 @@ const world = {
   renderer: null,
   clock: null,
   materials,
+  models: [],
   gridMesh: [],
 };
 
@@ -97,6 +98,12 @@ export default {
       innerHeight: 1080,
       controlsEnabled: true,
       grid: [],
+      drones: [
+        { x: 10, y: 10, model: 'drone' },
+      ],
+      farms: [
+        { x: 30, y: 30, model: 'farm' },
+      ],
     };
   },
   beforeMount() {
@@ -112,10 +119,10 @@ export default {
     if (world.renderer === null) {
       this.init();
       this.animate();
+      this.loadModels();
     }
     // this.getTile();
-    setInterval(this.tick, 1000);
-    this.renderGrid();
+    setInterval(this.tick, 5000);
   },
   watch: {
   },
@@ -126,6 +133,7 @@ export default {
   methods: {
     tick() {
       console.log('tick');
+      this.renderGrid();
     },
     createGrid(width, height) {
       const grid = [];
@@ -141,9 +149,34 @@ export default {
       // console.log(newMesh);
       // this.$store.dispatch('grid/setGrid', grid);
     },
+    setFarms() {
+      // world.farms.push({ x: 4, y: 3 });
+    },
+    renderDrones() {
+      const drone = this.drones[0];
+      drone.mesh = world.models[drone.model].clone();
+      const position = this.calcTile(drone.x, drone.y);
+      console.log(position, drone.mesh);
+      drone.mesh.position.x = position.x;
+      drone.mesh.position.z = position.y;
+      drone.mesh.position.y = 1;
+      world.scene.add(drone.mesh);
+    },
+    renderFarms() {
+      const farm = this.farms[0];
+      farm.mesh = world.models[farm.model].clone();
+      const position = this.calcTile(farm.x, farm.y);
+      console.log(position, farm.mesh);
+      farm.mesh.position.x = position.x;
+      farm.mesh.position.z = position.y;
+      farm.mesh.position.y = 1;
+      world.scene.add(farm.mesh);
+    },
     renderGrid() {
       // eslint-disable-next-line
       // Object.keys(this.grid).forEach((x) => {});this.setMesh(5, 5, this.getMesh('PLOT_INACTIVE'));
+      this.renderDrones();
+      this.renderFarms();
       Object.keys(tiles).forEach((i) => {
         this.grid[tiles[i].x][tiles[i].y].type = 'PLOT_INACTIVE';
         this.setMesh(tiles[i].x, tiles[i].y, this.getMesh('PLOT_INACTIVE'));
@@ -169,21 +202,35 @@ export default {
       return mesh;
     },
     calcTile(x, y) {
-      console.log(x, y);
       // eslint-disable-next-line
       return { x: ((x * this.worldFactor) - (this.worldScaleX / 2)), y: ((y * this.worldFactor) - (this.worldScaleY / 2)) };
     },
     getTile(x = 5, y = 5) {
       return this.grid[x][y];
     },
-    setTile(x = 5, y = 5) {
-      const gridTile = this.grid[x][y];
-      const texture = this.getMaterial(gridTile.type);
-      console.log(this.grid[x][y], texture);
-    },
+    // setTile(x = 5, y = 5) {
+    // const gridTile = this.grid[x][y];
+    // const texture = this.getMaterial(gridTile.type);
+    // console.log(this.grid[x][y], texture);
+    // },
     getMaterial(type = 'PLOT_INACTIVE') {
-      console.log(type);
       return typeof world.materials[type.toUpperCase()] !== 'undefined' ? world.materials[type.toUpperCase()].texture : 'sand.jpg';
+    },
+    async loadModels() {
+      world.models.farm = await this.loadModel('House/House_bamboo_state1.json', 1.5);
+      world.models.drone = await this.loadModel('drone.json', 1.5);
+    },
+    loadModel(filename, scale) {
+      return new Promise((resolve, reject) => {
+        try {
+          new THREE.ObjectLoader().load(`/static/models/${filename}`, (mesh) => {
+            mesh.scale.set(scale, scale, scale);
+            resolve(mesh);
+          });
+        } catch (e) {
+          reject(e);
+        }
+      });
     },
     init() {
       this.worldFactor = this.worldScaleX / this.worldWidth;
